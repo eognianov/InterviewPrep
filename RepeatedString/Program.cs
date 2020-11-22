3b66abc83cec105366c84db401b903331471e874
eognianov <emilian_89@abv.bg>
2020-11-22 20:37:29 +0200

    repeated string

---
+++ RepeatedString/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace RepeatedString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            long n = Convert.ToInt64(Console.ReadLine());
+
+            long result = repeatedString(s, n);
+
+            Console.WriteLine(result);
+        }
+
+        private static long repeatedString(string s, long n)
+        {
+            long size = s.Length;
+            long repeated = n/size;
+            long left = n - (size * repeated);
+            int extra = 0;
+
+            int count = 0;
+            for(int i = 0; i < size; i++){
+                if(s[i] == 'a'){
+                    ++count;
+                }
+            }
+
+            for(int i = 0; i < left; i++){
+                if(s[i] == 'a'){
+                    ++extra;
+                }
+            }
+
+            repeated = (repeated * count) + extra;
+
+            return repeated;
+        }
+    }
+}
