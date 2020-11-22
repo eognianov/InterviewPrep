a08b90df5fc6912f3c2cf6cca4485ac832ec0823
eognianov <emilian_89@abv.bg>
2020-11-22 20:11:20 +0200

    jumping on the clouds

---
+++ JumpingOnTheClouds/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace JumpingOnTheClouds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp))
+                ;
+            int result = jumpingOnClouds(c);
+
+            Console.WriteLine(result);
+        }
+
+        private static int jumpingOnClouds(int[] c)
+        {
+            int count = -1;
+            for (int i = 0; i < c.Length; i++, count++) {
+                if (i<c.Length-2 && c[i+2]==0) i++;
+            }
+
+            return count;
+        }
+    }
+}
