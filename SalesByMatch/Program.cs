a8f67054824d188551e686446e60d1c6b4b1cac5
eognianov <emilian_89@abv.bg>
2020-11-22 18:48:13 +0200

    Add project files.

---
+++ SalesByMatch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesByMatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+                ;
+            int result = sockMerchant(n, ar);
+
+            Console.WriteLine(result);
+
+        }
+
+        private static int sockMerchant(int n, int[] ar)
+        {
+            var socksByColor = new Dictionary<int, int>();
+
+            foreach (var sock in ar)
+            {
+                if (!socksByColor.ContainsKey(sock))
+                {
+                    socksByColor[sock] = 1;
+                    continue;
+                }
+
+                socksByColor[sock]++;
+            }
+
+            var result = 0;
+
+            foreach (var key in socksByColor.Keys)
+            {
+                result += socksByColor[key] / 2;
+            }
+
+            return result;
+        }
+    }
+}
